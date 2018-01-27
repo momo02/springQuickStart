38331ec8bb57a6098a1c59dc23da75576a709276
jung <jungha0902@naver.com>
2018-01-27 18:43:19 +0900

    Spring MVC 적용

--- BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -3,14 +3,16 @@ package com.springbook.view.board;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
 public class InsertBoardController implements Controller{
 
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 등록 처리");
 		
 		//1.사용자 입력 정보 추출
@@ -28,9 +30,10 @@ public class InsertBoardController implements Controller{
 		boardDAO.insertBoard(vo);
 		
 		//3.화면 네비게이션
-		//글 등록에 성공하면 등록된 글이 포함된 글 목록을 다시 검색해야 함.
-		//따라서 getBoardList.do 문자열을 리턴하여 리다이렉트 처리. 
-		return "getBoardList.do"; 
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:getBoardList.do");
+		return mav;
+		
 	}
 	
 	
